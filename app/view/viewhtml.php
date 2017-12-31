559ec5d44d7c2a06f9da06e7209978a86e49a764
Apacs <info@apacs.de>
2017-12-31 01:48:35 +0100

    view as html list

--- app/view/viewhtml.php
+++ app/view/viewhtml.php
@@ -4,12 +4,32 @@ class viewhtml
 {
     public function viewlist($dataobject)
     {
-      $html = '';
+      $html = '<h1>Datenbestand</h1>';
+      $html .= '<p>Eine pure Liste aller Objekte, die die Software gemäß Konfiguration abfragt und, sofern angegeben, die Preise berechnet.</p>';
+
+      $html .= '<table class="table table-striped">';
+      $html .= '<thead><tr>
+                  <th>Icon</th>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Current Price (calculated)</th>
+                </tr></thead>';
 
       foreach ($dataobject as $id => $item)
       {
-        $html .= $item->id;
+        $html .= '<tr>';
+
+        $html .= '<td><img src="' . $item->icon . '" /></td>';
+        $html .= '<td>' . $item->id . '</td>';
+        $html .= '<td>' . $item->name . '</td>';
+        $html .= '<td>' . $item->description . '</td>';
+        $html .= '<td>' . number_format($item->current->price, 0, ',', '.') . '</td>';
+
+        $html .= '</tr>';
       }
+
+      $html .= '</table>';
 
       return $html;
     }
